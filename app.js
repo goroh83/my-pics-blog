e03879e7f453f26285e43f4b524af0a92d24b6f8
goroh83 <goroh83@yahoo.com>
2017-06-29 18:53:01 +0000

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+var bodyParser = require('body-parser'),
+mongoose       = require('mongoose'),
+express        = require('express'),
+app            = express();
+
+
+// APP CONFIG
+mongoose.connect('mongodb://localhost/post');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//MONGOOSE MODEL CONFIG
+var postSchema = new mongoose.Schema ({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now}
+});
+
+var Post = mongoose.model('Post', postSchema); 
+
+
+
+// REST ROUTES
+
+// ap.get('/posts')
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('blog server is running');
+});
